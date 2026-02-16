3401598c94dbc15eecd34e3d440be808c864677f
パイピェーピョー <paipyepyo@gmail.com>
2026-02-16 23:55:18 +0900

    Create Printer.java

---
+++ small_programs/2.quotient_and_remainder/util/Printer.java
@@ -0,0 +1,11 @@
+package util;
+
+public class Printer {
+    public static void print(Object o) {
+        System.out.print(o);
+    }
+
+    public static void println(Object o) {
+        System.out.println(o);
+    }
+}
