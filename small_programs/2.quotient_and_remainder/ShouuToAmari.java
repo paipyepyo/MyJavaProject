8269db67c67b9da05f46c1728249118275652c2e
パイピェーピョー <paipyepyo@gmail.com>
2026-02-17 16:15:00 +0900

    Update ShouuToAmari.java

--- small_programs/2.quotient_and_remainder/ShouuToAmari.java
+++ small_programs/2.quotient_and_remainder/ShouuToAmari.java
@@ -16,21 +16,26 @@ public class ShouuToAmari {
     ArrayList<Num> list = new ArrayList<>();
     boolean stop = false;
     do {
+      // １.入力処理
       Num n = new Num(InputControl.getNumNoText("[ " + (list.size() + 1) + "番目 ]   : ",
-          "[ " + (list.size() + 1) + "番目 ] テキストではなく数字の入力をお願いします   : ", Double.class));
+          "[ " + (list.size() + 1) + "番目 ] テキストではなく半角の数字の入力をお願いします   : ", Double.class));
       list.add(n);
+      // 数字の個数が一つ以上であり、上記の命令で入力された数字はすぐ前の数字と同じである場合、ループを終了にします。
       if (list.size() > 1 && list.get(list.size() - 1).getNumber() == list.get(list.size() - 2).getNumber()) {
         stop = true;
       }
+
     } while (!stop);
     println("\nご入力ありがとうございました。");
     println("それぞれの商と余りは下記となります。\n");
 
+    // 2.表示処理
     int count = 0;
     for (Num num : list) {
       println("[ " + ++count + "番目 ]  : " + num);
     }
-
+    // リソースを閉じる
+    InputControl.close();
   }
 
 }
@@ -44,7 +49,7 @@ class Num {
 
   Num(double number) {
     this.number = number;
-    this.quotient = Double.valueOf(this.number).intValue();
+    this.quotient = Double.valueOf(this.number).intValue();// 商はオブジェクト作成時に決める
     this.remainder = findTheRemainder(this);
   }
 
@@ -57,6 +62,7 @@ class Num {
     return "数字：" + this.number + ",  商：" + this.quotient + ",   余り：" + this.remainder;
   }
 
+  // 数字を文字列に変換し、小数点を探し、余りを出す
   private static int findTheRemainder(Num n) {
     String s = String.valueOf(n.number);
     int decimalPointIndex = -1;
